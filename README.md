35dd8b302246b3036cf50f2b2d5ed0db1772a3c2
yijun <yijun2238@gmail.com>
2023-09-09 10:27:05 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# 仓库介绍
+记录本人学习技术的一些笔记，包括不局限于Java
+
+
+#
+
+# REF
+1. readMe工具：https://github.com/ekalinin/github-markdown-toc
+
---
+++ src/main/java/com/java/study/javastudy/multiThread/MultiTreadApp.java
@@ -0,0 +1,7 @@
+package com.java.study.javastudy.multiThread;
+
+public class MultiTreadApp {
+    public static void main(String[] args) {
+
+    }
+}
